31e4342c53a72df381a1b5d97112524c3abd3579
Cainan Whelchel <c.whelchel@gmail.com>
2024-03-26 22:38:13 -0400

    Save selected filters to and recall them from localStorage.
    Fix for #3

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -10,6 +10,7 @@ import './FilterBar.scss'
 import { createEqualityFilter, useAppContext } from '../AppContext';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import { ContextData } from '../../@types/ContextTypes';
 
 
 // https://mui.com/material-ui/react-table/
@@ -28,48 +29,49 @@ export const FilterBar = (props: IFilterBarPros) => {
 
     const { contextData, setData } = useAppContext();
 
+    // load up all the data stored in localStorage and use them... after the 
+    // API is ready
+    React.useEffect(() => {
+        window.addEventListener('pywebviewready', function () {
+            let bf = window.localStorage.getItem("BAND_FILTER") || '';
+            setBandFilter(bf);
+            let rf = window.localStorage.getItem("REGION_FILTER") || '';
+            setRegionFilter(rf);
+            let mf = window.localStorage.getItem("MODE_FILTER") || '';
+            setModeFilter(mf);
+            let lf = window.localStorage.getItem("LOCATION_FILTER") || '';
+            setLocationFilter(lf);
+
+            let qrtF = window.localStorage.getItem("QRT_FILTER");
+            setQrtFilter((qrtF === "true"));
+            let hf = window.localStorage.getItem("HUNTED_FILTER");
+            setHuntedFilter((hf === "true"));
+        });
+    },[]);
+
     const handleChange = (event: SelectChangeEvent) => {
         let m = event.target.value as string
-
-        setMode(m); // it doesn't work without this?????
-
-        contextData.filter.items = [];
-        contextData.filter.items.push(
-            createEqualityFilter('mode', m)
-        );
-
-        setData(contextData);
+        setModeFilter(m);
+        window.localStorage.setItem("MODE_FILTER", m);
     };
 
     const handleBandChange = (event: SelectChangeEvent) => {
         let m = event.target.value as string;
-        let x = parseInt(m);
-        console.log("changing band to: " + m);
-        window.pywebview.api.set_band_filter(x);
-
-        let next = { ...contextData, bandFilter: x };
-        setData(next);
-        setBand(m);
+        setBandFilter(m);
+        window.localStorage.setItem("BAND_FILTER", m);
     }
 
     const handleRegionChange = (event: SelectChangeEvent) => {
         let r = event.target.value as string;
-        console.log("changing region to: " + r);
-        window.pywebview.api.set_region_filter(r);
+        setRegionFilter(r);
 
-        let next = { ...contextData, regionFilter: r };
-        setData(next);
-        setRegion(r);
+        window.localStorage.setItem("REGION_FILTER", r);
     }
 
     const handleLocationChange = (event: SelectChangeEvent) => {
         let l = event.target.value as string;
-        console.log("changing location to: " + l);
-        window.pywebview.api.set_location_filter(l);
-
-        let next = { ...contextData, locationFilter: l };
-        setData(next);
-        setLocation(l);
+        setLocationFilter(l);
+        window.localStorage.setItem("LOCATION_FILTER", l);
     }
 
     const handleClear = () => {
@@ -81,14 +83,41 @@ export const FilterBar = (props: IFilterBarPros) => {
         );
         window.pywebview.api.set_band_filter(0);
         window.pywebview.api.set_region_filter("");
+        window.pywebview.api.set_qrt_filter(true);
+        window.pywebview.api.set_hunted_filter(false);
 
-        let next = { ...contextData, bandFilter: 0, regionFilter: "", locationFilter: "" };
+        let next = { ...contextData, 
+            bandFilter: 0, 
+            regionFilter: "", 
+            locationFilter: "", 
+            qrtFilter: true, 
+            huntedFilter: false};
         setData(next);
         setRegion("");
         setLocation("");
+        setQrt(true);
+        setHunted(false);
+
+        window.localStorage.setItem("BAND_FILTER", '0');
+        window.localStorage.setItem("REGION_FILTER",'');
+        window.localStorage.setItem("MODE_FILTER", '');
+        window.localStorage.setItem("LOCATION_FILTER", '');
+        window.localStorage.setItem("QRT_FILTER", 'true');
+        window.localStorage.setItem("HUNTED_FILTER", 'false');
+
     };
 
     function handleQrtSwitch(event: any, checked: boolean): void {
+        setQrtFilter(checked);
+        window.localStorage.setItem("QRT_FILTER", checked.toString());
+    }
+
+    function handleHuntedSwitch(event: any, checked: boolean): void {
+        setHuntedFilter(checked);
+        window.localStorage.setItem("HUNTED_FILTER", checked.toString());
+    }
+    
+    function setQrtFilter(checked: boolean) {
         console.log("changing qrt filter to: " + checked);
         window.pywebview.api.set_qrt_filter(checked);
 
@@ -97,13 +126,52 @@ export const FilterBar = (props: IFilterBarPros) => {
         setQrt(checked);
     }
 
-    function handleHuntedSwitch(event: any, checked: boolean): void {
+    function setHuntedFilter(checked: boolean) {
         console.log("changing hunted filter to: " + checked);
         window.pywebview.api.set_hunted_filter(checked);
 
         let next = { ...contextData, huntedFilter: checked };
         setData(next);
         setHunted(checked);
+    }
+
+    function setModeFilter(m: string) {
+        setMode(m); // it doesn't work without this?????
+
+        contextData.filter.items = [];
+        contextData.filter.items.push(
+            createEqualityFilter('mode', m)
+        );
+
+        setData(contextData);
+    }
+
+    function setBandFilter(m: string) {
+        let x = parseInt(m);
+        console.log("changing band to: " + m);
+        window.pywebview.api.set_band_filter(x);
+    
+        let next = { ...contextData, bandFilter: x };
+        setData(next);
+        setBand(m);
+    }
+    
+    function setRegionFilter(r: string) {
+        console.log("changing region to: " + r);
+        window.pywebview.api.set_region_filter(r);
+
+        let next = { ...contextData, regionFilter: r };
+        setData(next);
+        setRegion(r);
+    }
+
+    function setLocationFilter(l: string) {
+        console.log("changing location to: " + l);
+        window.pywebview.api.set_location_filter(l);
+
+        let next = { ...contextData, locationFilter: l };
+        setData(next);
+        setLocation(l);
     }
 
     return (
@@ -206,3 +274,5 @@ export const FilterBar = (props: IFilterBarPros) => {
 
     );
 }
+
+
