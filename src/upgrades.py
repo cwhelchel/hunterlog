6d2583808dc14ba5bd09260d5c4f9c1ca1f914ff
Cainan Whelchel <c.whelchel@gmail.com>
2025-02-12 07:43:23 -0500

    Add auto-backup for spots.db
    Fixes #63

--- src/upgrades.py
+++ src/upgrades.py
@@ -1,3 +1,5 @@
+import time
+import shutil
 from alembic_src import versions
 import logging as L
 
@@ -8,6 +10,16 @@ logging = L.getLogger(__name__)
 
 def do_upgrade():
     logging.info('upgrading to head')
+
+    # backup spot file
+    try:
+        logging.info('backing up spots.db')
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        fn = f"spots-{timestamp}.db-back"
+        shutil.copy2("spots.db", fn)
+    except Exception as ex:
+        logging.error("Couldnot back up spots.db", exc_info=ex)
+
     versions.upgrade()
 
 
