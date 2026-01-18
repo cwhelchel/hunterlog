62d649ed8629fe92bacd64ead2cc9b9bb3c5fa95
Cainan Whelchel <c.whelchel@gmail.com>
2026-01-18 12:33:50 -0500

    Move Stats/ -> AppMenu/MenuItems/Stats

--- src/components/AppMenu/MenuItems/Stats/HamAlert.tsx
+++ src/components/AppMenu/MenuItems/Stats/HamAlert.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { Stack } from '@mui/system';
 import { Tooltip, Button, Divider, Typography, Autocomplete, TextField, Snackbar, SnackbarCloseReason } from '@mui/material';
-import { checkApiResponse } from '../Utilities/util';
-import { useAppContext } from '../AppContext';
-import HlModal2 from '../Common/HlModal';
+import { checkApiResponse } from '../../../Utilities/util';
+import { useAppContext } from '../../../AppContext';
+import HlModal2 from '../../../Common/HlModal';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 declare interface IHamAlertButtonProps {
--- src/components/AppMenu/MenuItems/Stats/ImportAdif.tsx
+++ src/components/AppMenu/MenuItems/Stats/ImportAdif.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { Tooltip, Button } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
-import { checkApiResponse } from '../Utilities/util';
-import { useAppContext } from '../AppContext';
+import { checkApiResponse } from '../../../Utilities/util';
+import { useAppContext } from '../../../AppContext';
 
 declare interface IImportAdifProps {
     setIsWorking: (val: boolean) => void,
