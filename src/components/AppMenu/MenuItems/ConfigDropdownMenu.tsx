3f44e98033237bf25e7ed390f5f223691a541287
Cainan Whelchel <c.whelchel@gmail.com>
2026-02-03 13:13:16 -0500

    bugfix: Fix focus issue with settings modals.
    Typing certain characters in the Settings modals would try to focus on the
    open Settings menu. Prevent that by stopping key press propagation from
    the menu items

--- src/components/AppMenu/MenuItems/ConfigDropdownMenu.tsx
+++ src/components/AppMenu/MenuItems/ConfigDropdownMenu.tsx
@@ -9,22 +9,31 @@ import ConfigModal from './SettingsMenu/Config/ConfigModal';
 export default function ConfigDropdownMenu() {
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
+    const openMenu = Boolean(anchorEl);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
     };
+
     const handleClose = () => {
         setAnchorEl(null);
+    };
+
+    // when modals popup from the menuitems, the open menu tries to handle the
+    // any key presses where the key pressed is the first letter of the menu item.
+    // so A for alerts or C for configuration / callsign notes. In order to type
+    // those letters in the popup modals we have to stop the event from propagating
+    const avoidBubblingUp = (e: React.KeyboardEvent) => {
+        e.stopPropagation();
     };
 
     return (
         <div>
             <Button
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
+                id="config-menu-button"
+                aria-controls={openMenu ? 'config-menu' : undefined}
                 aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
+                aria-expanded={openMenu ? 'true' : undefined}
                 onClick={handleClick}
                 style={{
                     color: "#bdbdbd"
@@ -33,24 +42,24 @@ export default function ConfigDropdownMenu() {
                 Settings
             </Button>
             <Menu
-                id="basic-menu"
+                id="config-menu"
                 anchorEl={anchorEl}
-                open={open}
+                open={openMenu}
                 onClose={handleClose}
                 MenuListProps={{
                     'aria-labelledby': 'basic-button',
                 }}
             >
-                <MenuItem >
+                <MenuItem onKeyDown={avoidBubblingUp} >
                     <ConfigContextProvider>
                         <ConfigModal />
                     </ConfigContextProvider>
                 </MenuItem>
                 <Divider />
-                <MenuItem >
+                <MenuItem onKeyDown={avoidBubblingUp} >
                     <AlertsMenu />
                 </MenuItem>
-                <MenuItem >
+                <MenuItem onKeyDown={avoidBubblingUp} >
                     <CallNotesMenu />
                 </MenuItem>
             </Menu>
