247924d9ad6a4adeb1c383211d33807d2460c0e1
Cainan Whelchel <c.whelchel@gmail.com>
2026-08-13 11:52:10 -0400

    Macos fix try again

--- build-macos.py
+++ build-macos.py
@@ -4,9 +4,10 @@ import shutil
 
 from distutils.core import setup
 
+
 def tree(src):
     return [(root, map(lambda f: os.path.join(root, f), files))
-        for (root, dirs, files) in os.walk(os.path.normpath(src))]
+            for (root, dirs, files) in os.walk(os.path.normpath(src))]
 
 
 if os.path.exists('build'):
@@ -32,7 +33,12 @@ OPTIONS = {
     'plist': {
         'NSRequiresAquaSystemAppearance': False
     },
-    'resources': DATA_FILES
+    'resources': DATA_FILES,
+    #  GH action error. cant code sign these libs so exclude them (testing)
+    'dylib_excludes': [
+        "/Library/Frameworks/Python.framework/Versions/3.14/Frameworks/Tcl.framework",
+        "/Library/Frameworks/Python.framework/Versions/3.14/Frameworks/Tk.framework",
+    ],
 }
 
 setup(
@@ -40,10 +46,5 @@ setup(
     data_files=HL_DATA,
     name='Hunterlog',
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-    #  GH action error. cant code sign these libs so exclude them (testing)
-    dylib_excludes=[
-        "/Library/Frameworks/Python.framework/Versions/3.14/Frameworks/Tcl.framework",
-        "/Library/Frameworks/Python.framework/Versions/3.14/Frameworks/Tk.framework",
-    ],
+    setup_requires=['py2app']
 )
